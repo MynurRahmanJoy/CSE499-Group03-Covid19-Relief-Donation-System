4a7ecd36756a39f16f4199b64700e1012778c27b
sanjida12345 <56125108+sanjida12345@users.noreply.github.com>
2021-08-30 00:48:29 +0600

    Add files via upload

---
+++ CSE 499 (Final Project)/navbar.php
@@ -0,0 +1,34 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg p-3 mb-5 ">
+  <a class="navbar-brand" href="#">Life Shield</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="index.php" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Please Login To Continue <span class="sr-only">(current)</span></a>
+      </li>
+     
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Login Here
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="login/adminLogin.php">Admin</a>
+          <a class="dropdown-item" href="login/donorLogin.php">Donor</a>
+          <a class="dropdown-item" href="login/volunteerLogin.php">Volunteer</a>
+          <a class="dropdown-item" href="https://docs.google.com/forms/d/e/1FAIpQLScZ74rCZCMsy0C8m7i4sgIp_tcscOA-LIh0mwBcwQ_Rb9X5Fw/viewform?usp=sf_link">Seeker</a>
+
+          <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Task Panel
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="http://localhost/vol/admin/AdminLogin.php">Admin</a>
+          <a class="dropdown-item" href="http://localhost/vol/index.php">Volunteer</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
